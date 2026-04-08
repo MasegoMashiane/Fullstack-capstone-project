2440e7273c4a2bf3407958095dfd4500b8fb5313
Masego <mashianemlb@gmail.com>
2026-04-08 10:58:35 -0400

    deployed with the details ednpoint failing testing

--- giftwebsite/index.js
+++ giftwebsite/index.js
@@ -4,7 +4,15 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
 app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('/app', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
